1e62b4b92fe9f3543803f4f214704fe18e47e951
Alberto <a.artiedalopez@gmail.com>
2023-02-01 12:48:19 +0100

    changes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine
+import classes
+from config import *
+
+engine = create_engine(f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}")
 
 app = FastAPI()
+
+print(engine)
 
 @app.get("/health")
 def health_check():
     return "OK"
+
+
