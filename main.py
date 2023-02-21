72490456c8a9c84f38eade24485add4836ad8af3
Alberto <a.artiedalopez@gmail.com>
2023-02-21 13:12:46 +0100

    ..

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def getProvincias():
             select(Provincia.provincia).order_by(Provincia.ID_provincia)
             ).all()
 
-@app.post("/givenBooks")
+@app.post("/givenBooks", status_code=status.HTTP_201_CREATED)
 def givenBooks(usuario: Login):
     with Session(engine) as session:
         user = session.exec(
