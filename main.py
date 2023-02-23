aa6d358c03dab191cae86318a823b9a5ae67f019
Alberto <a.artiedalopez@gmail.com>
2023-02-23 11:46:29 +0100

    ..

--- main.py
+++ main.py
@@ -64,10 +64,9 @@ def givenBooks(token: str = Header(default=None)):
         user = session.exec(
             select(Usuarios).where(Usuarios.token == token)
             ).one()
-        libro = session.exec(
+        return session.exec(
             select(Libros.imagen_libro,Libros.titulo,Libros.isbn,Cambios.fecha).where(Cambios.ID_user_vende == user.ID_usuario, Libros.ID_libro == Cambios.ID_libro)
             ).all()
-        return libro
 
 # @app.get("/givenBooks")
 # def givenBooks():
