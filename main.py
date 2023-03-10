1b40fb9df1616081fb141b241d3f7830e093cad0
Alberto <a.artiedalopez@gmail.com>
2023-03-10 12:59:00 +0100

    ..

--- main.py
+++ main.py
@@ -73,17 +73,11 @@ def change(cambio: Cambios, user : Usuarios = Depends(comprobarUser)):
         cambio.fecha = datetime.now()
 
         # Arreglar los cambios en las tablas
-        # session.add(user, users_vende, cambio, changed_book)
         session.add(user)
         session.add(users_vende)
         session.add(cambio)
         session.add(changed_book)
         session.commit()
-        # session.refresh(user, users_vende, cambio, changed_book)
-        session.refresh(user)
-        session.refresh(users_vende)
-        session.refresh(cambio)
-        session.refresh(changed_book)
 
 # Devuelve todos los nombres de las provincias que hay en BBDD para usarlos en un DropDownList de la app
 @app.get("/getProvincias")
